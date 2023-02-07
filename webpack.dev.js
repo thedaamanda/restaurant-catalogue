11e170686f2e21431fa34a6671813f08865a0dd2
thedaamanda <thedaamandaa@gmail.com>
2023-02-07 08:52:00 +0700

    fix: target anchor from reviewer

--- webpack.dev.js
+++ webpack.dev.js
@@ -8,7 +8,7 @@ module.exports = merge(common, {
     devServer: {
         static: path.resolve(__dirname, 'dist'),
         open: true,
-        port: 8081,
+        port: 9000,
         client: {
             overlay: {
                 errors: true,
