9605a7acfed310e3fd6dd0e00a3ca1d6acc2cd46
thedaamanda <thedaamandaa@gmail.com>
2023-02-23 00:30:51 +0700

    feat: add manifest, service worker, cache API and Workbox

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,6 +1,8 @@
 const CONFIG = {
   BASE_URL: 'https://restaurant-api.dicoding.dev/',
   BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/medium/',
+  DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: new Date().toISOString(),
 };
 
 export default CONFIG;
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,6 +3,7 @@ import '../styles/main.scss';
 import '../styles/detail.scss';
 import '../styles/responsive.scss';
 import App from './views/app';
+import swRegister from './utils/sw-register';
 
 const app = new App({
   button: document.querySelectorAll('a.open_close'),
@@ -19,4 +20,5 @@ window.addEventListener('hashchange', () => {
 
 window.addEventListener('load', () => {
   app.renderPage();
+  swRegister();
 });
