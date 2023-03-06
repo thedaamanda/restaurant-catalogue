56eb4c78f73f61db5f154a5e7d55371b891bb5c5
thedaamanda <thedaamandaa@gmail.com>
2023-03-06 22:20:54 +0700

    feat: add image responsive

--- webpack.common.js
+++ webpack.common.js
@@ -74,10 +74,10 @@ module.exports = {
         }),
         new CopyWebpackPlugin({
             patterns: [
-                {
-                    from: path.resolve(__dirname, 'src/public/'),
-                    to: path.resolve(__dirname, 'dist/'),
-                },
+              {
+                from: path.resolve(__dirname, 'src/public'),
+                to: path.resolve(__dirname, 'dist'),
+              },
             ],
         }),
         new WebpackPwaManifest({
